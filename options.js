76a32bed56aef39621c64e1c461cfefdd3c31745
awh <295185023@qq.com>
2018-08-29 21:42:35 +0800

    third

--- options.js
+++ options.js
@@ -13,3 +13,73 @@ let page = document.getElementById('buttonDiv');
     }
   }
   constructOptions(kButtonColors);
+  
+  
+  
+//----------------------------------------------------------------------
+  //from Google Developer Extensions Options Tutorial
+// Saves options to chrome.storage
+function save_options() {
+	var options = new Array(20);
+	var i;
+	for(i = 1; i <= options.length; i++){
+		options[i-1] = document.getElementById('website'+i).value;
+	}
+  chrome.storage.sync.set({
+    option: options,
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved! Thank you.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 1750);
+  });
+}
+
+//Restores select box and checkbox state using the preferences
+//stored in chrome.storage.
+function restore_options() {
+// Use default value color = 'red' and likesColor = true.   ???????????????get?set?
+chrome.storage.sync.get({
+ option: ["zhihu.com/","bilibili.com/","baidu.com/","reddit.com/","pinterest.com/","vimeo.com/","plus.google","tumblr.com/","instagram.com/","","","","","","","","","","",""]
+}, function(items) {
+	var x;
+	for(x = 1; x <= 20; x++){
+		document.getElementById('website'+x).value = items.option[x-1];
+	}
+});
+}
+
+function reset(){
+	var resetArray = ["zhihu.com/","bilibili.com/","baidu.com/","reddit.com/","pinterest.com/","vimeo.com/","plus.google","tumblr.com/","instagram.com/","","","","","","","","","","",""];
+	var x;
+	for(x = 1; x <= 20; x++){
+		document.getElementById('website'+x).value = resetArray[x-1];
+	}
+}
+
+
+//
+document.addEventListener('DOMContentLoaded', restore_options);
+var resetbtn = document.getElementById('reset');
+if(resetbtn){
+	resetbtn.addEventListener('click',reset);
+}
+var svbtn = document.getElementById('save');
+if(svbtn){
+  svbtn.addEventListener('click',
+    save_options);
+}
+
+let close = document.getElementById('close');
+close.onclick = function(element) {
+	chrome.storage.sync.set({control_button: false}, function() {
+	    console.log("control_button: false");
+	    var status = document.getElementById('status');
+	    status.textContent = 'close!';
+	    setTimeout(function() {
+	      status.textContent = '';
+	    }, 1000);
+	  });
+};
--- popup.js
+++ popup.js
@@ -13,3 +13,71 @@
           {code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
   };
+  
+  
+
+//=======================display===========================================================
+
+//chrome.storage.sync.get('control_button', function(data) {
+//	let ibool = data.control_button;
+//	document.getElementById('state').textContent = "control " + ibool;
+//});
+
+//
+let open = document.getElementById('open');
+open.onclick = function(element) {
+	chrome.storage.sync.set({control_button: true}, function() {
+	    console.log("control_button: true");
+	    var status = document.getElementById('status');
+	    status.textContent = 'Open! Thank you.';
+	    setTimeout(function() {
+	      status.textContent = '';
+	    }, 1000);
+	  });
+};
+
+
+
+
+let fiveMin = document.getElementById('fiveMin');
+fiveMin.onclick = function(element) {
+	chrome.storage.sync.set({control_button: false}, function() {
+	    console.log("fiveMin");
+	    
+		var alarmName = 'remindme';
+		chrome.alarms.create(alarmName, {delayInMinutes: 5});
+		
+		chrome.notifications.create('fiveMin', {
+        type: 'basic',
+        iconUrl: 'images/get_started16.png',
+        title: 'fiveMin',
+        message: 'fiveMin!'
+    }, function(notificationId) {});
+		
+		
+		var status = document.getElementById('status');
+	    status.textContent = 'fiveMin!';
+	    setTimeout(function() {
+		      status.textContent = '';
+		    }, 1000);
+	  });
+};
+
+
+let fiveMin = document.getElementById('tMin');
+fiveMin.onclick = function(element) {
+	chrome.storage.sync.set({control_button: false}, function() {
+	    console.log("fiveMin");
+	    
+		var alarmName = 'remindme';
+		chrome.alarms.create(alarmName, {delayInMinutes: 30});
+		
+		var status = document.getElementById('status');
+	    status.textContent = '30 Min!';
+	    setTimeout(function() {
+		      status.textContent = '';
+		    }, 1000);
+	  });
+};
+
+  
